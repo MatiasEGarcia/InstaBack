2241139bbb68f644de63f3819a0e0c2bd61451f3
Matias.E.Garcia <matiasegarcia22@gmail.com>
2023-02-10 15:34:51 -0300

    first commit

---
+++ src/main/java/com/instaJava/instaJava/InstaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.instaJava.instaJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/instaJava/instaJava/InstaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instaJava.instaJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
