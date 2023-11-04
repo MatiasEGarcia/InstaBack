246d7dd74fd16510ad9a98615b148980279c5ebf
Matias.E.Garcia <matiasegarcia22@gmail.com>
2023-11-03 21:09:58 -0300

    hanldeWebSocketDisconnectListener

--- src/main/java/com/instaJava/instaJava/config/WebSocketEventListener.java
+++ src/main/java/com/instaJava/instaJava/config/WebSocketEventListener.java
@@ -1,24 +1,20 @@
 package com.instaJava.instaJava.config;
 
 import org.springframework.context.event.EventListener;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
 public class WebSocketEventListener {
 
 	@EventListener
 	public void hanldeWebSocketDisconnectListener(SessionDisconnectEvent event) {
-		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-		String username = (String) headerAccessor.getSessionAttributes().get("username");
-		if(username != null) {
-			log.info("User disconnected : {}", username);
-		}
+		// Handle WebSocket disconnect event
+        String sessionId = event.getSessionId();
+		log.info("WebSocket session disconnected : {}", sessionId);
+
 	}
 }
